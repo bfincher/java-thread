e12b458c0bda1b311c33a7cf42e2375d07ac2034
bfincher <brian@fincherhome.com>
2019-10-12 09:50:36 -0500

    Checkstyle (#2)
    * wip on checkstyle
    
    * checkstyle

--- src/main/java/com/fincher/thread/CallableWithIdIfc.java
+++ src/main/java/com/fincher/thread/CallableWithIdIfc.java
@@ -2,12 +2,12 @@ package com.fincher.thread;
 
 import java.util.concurrent.Callable;
 
-/** A Java Callable with an added identifier
+/** A Java Callable with an added identifier.
  * 
  * @author Brian Fincher
  *
- * @param <T>
+ * @param <T> see Callable
  */
-public interface CallableWithIdIfc <T> extends Callable<T>, IdentifiableIfc {
+public interface CallableWithIdIfc<T> extends Callable<T>, IdentifiableIfc {
 
 }
--- src/main/java/com/fincher/thread/DataHandlerIfc.java
+++ src/main/java/com/fincher/thread/DataHandlerIfc.java
@@ -1,18 +1,18 @@
 package com.fincher.thread;
 
-/** Used as a callback mechanism to notify message listeners of available data
+/** Used as a callback mechanism to notify message listeners of available data.
  * 
  * @author Brian Fincher
  *
- * @param <T>
+ * @param <T> The type of data
  */
 
-public interface DataHandlerIfc <T> {			
-		
-	/** A message has arrived that should be processed
-	 * 
-	 * @param data The message to be processed
-	 */
-	public void handleMessage(T data);
+public interface DataHandlerIfc<T> {
+
+    /** A message has arrived that should be processed.
+     * 
+     * @param data The message to be processed
+     */
+    public void handleMessage(T data);
 
 }
